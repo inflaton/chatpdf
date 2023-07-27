784131a3bbbcbd86ed0bc3176d47889772b5ca5c
Donghao Huang <inflaton.sg@gmail.com>
2023-07-27 17:45:42 +0800

    updated scripts

--- scripts/llama-2.sh
+++ scripts/llama-2.sh
@@ -2,11 +2,12 @@
 
 BASEDIR=$(dirname "$0")
 cd $BASEDIR/..
+echo Current Directory:
 pwd
 
 export TRANSFORMERS_CACHE=/common/scratch/users/d/dh.huang.2023/transformers
 
-EXT=cluster
+EXT=cluster_a40
 
 export HUGGINGFACE_MODEL_NAME_OR_PATH="meta-llama/Llama-2-7b-chat-hf"
 echo Testing $HUGGINGFACE_MODEL_NAME_OR_PATH
